92d32124b36af2ca3733f99a80932853c5a5750f
Ch0min <marklundgaard7@gmail.com>
2021-12-05 20:37:47 +0100

    up

--- ICE Project December 2021/src/Main.java
+++ ICE Project December 2021/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-      //  StartMenu startMenu = new StartMenu();  // Bruges lidt som vores controller class.
-        GameDesign gameDesign = new GameDesign();     // Skal ikke være her til sidst. Bruges kun til at lave Gamedesign så menuen ikke popper op først hele tiden.
+        StartMenu startMenu = new StartMenu();  // Bruges lidt som vores controller class.
+      //  GameDesign gameDesign = new GameDesign();     // Skal ikke være her til sidst. Bruges kun til at lave Gamedesign så menuen ikke popper op først hele tiden.
       //  ResultsWindow rw = new ResultsWindow();    //   Samme som ^
 
 
@@ -14,13 +14,15 @@ public class Main {
 
 
 /* NOTER TIL NÆSTE GANG
-- Pop result window, 'Knappen Pengecheck' skal åbne nyt vindue. //done
-- Få resultwindow op, når man har tabt.                         //done
+- Pop result window, 'Knappen Pengecheck' skal åbne nyt vindue.       //done
+- Få resultwindow op, når man har tabt.                               //done
 - Når timeren rammer 0, skal der lyd på
 - resultwindow popup vindue skal designes
 - pengebeløb popup vindue skal designes
 - resultwindow skal der ny lyd til
 - Check om hvis man får en million at pengecheck popup vindue kommer frem
-- Højre side i gamedesign skal designes
+- Højre side i gamedesign skal designes  /livslinjer, et træ af pengebeløb så man kan se hvor man er nået.
 - millionwindow skal designes og laves lyd til
+-
+-
  */
